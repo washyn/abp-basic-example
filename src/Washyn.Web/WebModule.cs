03849151bfc8d27c53509c5e667fdbaa8461b25b
Washyn <shin321654987@gmail.com>
2021-10-06 19:36:38 -0700

    added web module

--- src/Washyn.Web/WebModule.cs
+++ src/Washyn.Web/WebModule.cs
@@ -2,13 +2,61 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Volo.Abp;
 using Volo.Abp.Modularity;
 using Washyn.Application;
+using Washyn.EntityFrameworkCore;
 
 namespace Washyn.Web
 {
-    [DependsOn(typeof(ApplicationModule))]
-    public class WebModule :AbpModule
+    [DependsOn(typeof(ApplicationModule),
+        typeof(EntityFrameworkCoreModule))]
+    public class WebModule : AbpModule
     {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            // base.ConfigureServices(context);
+
+            var hostingEnvironment = context.Services.GetHostingEnvironment();
+            var configuration = context.Services.GetConfiguration();
+
+            context.Services.AddControllersWithViews();
+        }
+
+        public override void OnApplicationInitialization(ApplicationInitializationContext context)
+        {
+            // base.OnApplicationInitialization(context);
+
+            var app = context.GetApplicationBuilder();
+            var env = context.GetEnvironment();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
     }
 }
