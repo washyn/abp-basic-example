fc95e06617b674127c6098c4a999f610159a4f60
Washington Acero M <shin321654987@gmail.com>
2022-07-29 11:57:14 -0700

    extracted brand logo

---
+++ others/Washyn.AdminLteTheme/Themes/AdminLte/Components/Brand/BrandLogoViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Washyn.AdminLteTheme.Themes.AdminLte.Components.Brand
+{
+    public class BrandLogoViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("~/Themes/AdminLte/Components/Brand/Default.cshtml");
+        }
+    }
+}
