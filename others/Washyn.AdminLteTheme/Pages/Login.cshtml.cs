1d00dfc1463575026a037001602f3f7a1d59578c
Washington Acero M <shin321654987@gmail.com>
2022-07-29 12:45:28 -0700

    aded admin lte login example page

---
+++ others/Washyn.AdminLteTheme/Pages/Login.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Washyn.AdminLteTheme.Pages
+{
+    public class Login : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
