8d8155e7223eb9a7b6c4315e6e189bae1780c535
Washington Acero M <shin321654987@gmail.com>
2022-07-29 12:10:18 -0700

    aded custom brand for test component provider

---
+++ others/Washyn.AdminLteTheme/CustomAppBrandingProvider.cs
@@ -0,0 +1,11 @@
+﻿using Volo.Abp.DependencyInjection;
+using Volo.Abp.Ui.Branding;
+
+namespace Washyn.AdminLteTheme
+{
+    [Dependency(ReplaceServices = true)]
+    public class CustomAppBrandingProvider : DefaultBrandingProvider
+    {
+        public override string AppName => "Custom brand prov";
+    }
+}
