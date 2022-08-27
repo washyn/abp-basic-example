3f8d409b0adb39d47a838ff7d09db2271857835d
Washington Acero M <shin321654987@gmail.com>
2022-08-26 23:24:55 -0700

    aded proyect fot test adminlte

---
+++ others/Washyn.Internet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Washyn.Internet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
