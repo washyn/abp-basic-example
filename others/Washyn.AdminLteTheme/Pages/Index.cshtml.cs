11bb5431f7b51d2400141b489b5ed1e8105f9f84
Washington Acero M <shin321654987@gmail.com>
2022-08-26 21:14:39 -0700

    aded part of toolbar

--- others/Washyn.AdminLteTheme/Pages/Index.cshtml.cs
+++ others/Washyn.AdminLteTheme/Pages/Index.cshtml.cs
@@ -8,15 +8,15 @@ namespace Washyn.AdminLteTheme.Pages
     {
         public void OnGet()
         {
-            Alerts.Add(new AlertMessage(AlertType.Default, "Texto de la alerta.", "Titulo", true));
-            Alerts.Add(new AlertMessage(AlertType.Primary, "Texto de la alerta.", "Titulo", true));
-            Alerts.Add(new AlertMessage(AlertType.Secondary, "Texto de la alerta.", "Titulo", true));
-            Alerts.Add(new AlertMessage(AlertType.Success, "Texto de la alerta.", "Titulo", true));
-            Alerts.Add(new AlertMessage(AlertType.Danger, "Texto de la alerta.", "Titulo", true));
-            Alerts.Add(new AlertMessage(AlertType.Warning, "Texto de la alerta.", "Titulo", true));
+            // Alerts.Add(new AlertMessage(AlertType.Default, "Texto de la alerta.", "Titulo", false));
+            // Alerts.Add(new AlertMessage(AlertType.Primary, "Texto de la alerta.", "Titulo", false));
+            // Alerts.Add(new AlertMessage(AlertType.Secondary, "Texto de la alerta.", "Titulo", false));
+            // Alerts.Add(new AlertMessage(AlertType.Success, "Texto de la alerta.", "Titulo", false));
+            // Alerts.Add(new AlertMessage(AlertType.Danger, "Texto de la alerta.", "Titulo", false));
+            // Alerts.Add(new AlertMessage(AlertType.Warning, "Texto de la alerta.", "Titulo", false));
             Alerts.Add(new AlertMessage(AlertType.Info, "Texto de la alerta.", "Titulo", true));
-            Alerts.Add(new AlertMessage(AlertType.Light, "Texto de la alerta.", "Titulo", true));
-            Alerts.Add(new AlertMessage(AlertType.Dark, "Texto de la alerta.", "Titulo", true));
+            // Alerts.Add(new AlertMessage(AlertType.Light, "Texto de la alerta.", "Titulo", false));
+            // Alerts.Add(new AlertMessage(AlertType.Dark, "Texto de la alerta.", "Titulo", false));
         }
     }
 }
