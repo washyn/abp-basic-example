54570448b2b7d4effd45402615df5a1951ed58db
Washington Acero M <shin321654987@gmail.com>
2022-08-31 10:57:34 -0700

    aded test empty layout

--- others/Washyn.AdminLteTheme/AdminLteTheme.cs
+++ others/Washyn.AdminLteTheme/AdminLteTheme.cs
@@ -10,6 +10,7 @@ namespace Washyn.AdminLteTheme
 
         public string GetLayout(string name, bool fallbackToDefault = true)
         {
+            // AdminLteEmptyLayout
             switch (name)
             {
                 case StandardLayouts.Application:
@@ -18,6 +19,9 @@ namespace Washyn.AdminLteTheme
                     return "~/Themes/AdminLte/Layouts/Account.cshtml";
                 case StandardLayouts.Empty:
                     return "~/Themes/AdminLte/Layouts/Empty.cshtml";
+                case "AdminLteEmptyLayout":
+                    return "~/Themes/AdminLte/Layouts/AdminLteEmptyLayout.cshtml";
+                
                 default:
                     return fallbackToDefault ? "~/Themes/AdminLte/Layouts/Application.cshtml" : null;
             }
---
+++ others/Washyn.Internet/Pages/Empty.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Washyn.Internet.Pages
+{
+    public class Empty : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
