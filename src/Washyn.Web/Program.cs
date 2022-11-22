88c01becc222701c3914e5398e2e663b6b7f9140
Washington Acero M <shin321654987@gmail.com>
2022-11-21 18:50:11 -0800

    aded serilog

--- src/Washyn.Web/Program.cs
+++ src/Washyn.Web/Program.cs
@@ -6,14 +6,45 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace Washyn.Web
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var logger = new LoggerConfiguration()
+#if DEBUG
+                .MinimumLevel.Debug()
+#else
+                .MinimumLevel.Information()
+#endif
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Async(configuration => configuration.File("Logs/log.log"))
+#if DEBUG
+                .WriteTo.Async(configuration => configuration.Console())
+#endif
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host.");
+                CreateHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Host terminated!");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,6 +53,11 @@ namespace Washyn.Web
                 {
                     webBuilder.UseStartup<Startup>();
                 })
-                .UseAutofac();
+                .ConfigureAppConfiguration(builder =>
+                {
+                    builder.AddJsonFile("appsettings.secrets.json", optional: true);
+                })
+                .UseAutofac()
+                .UseSerilog();
     }
 }
