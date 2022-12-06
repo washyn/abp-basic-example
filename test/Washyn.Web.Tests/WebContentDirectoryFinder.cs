a2d9d6d06ed9edde18bbf456cb90f983620ac7a5
Washington Acero M <shin321654987@gmail.com>
2022-12-05 21:17:05 -0800

    aded web test example

---
+++ test/Washyn.Web.Tests/WebContentDirectoryFinder.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Washyn.Domain;
+
+namespace Washyn.Web.Tests
+{
+    public class WebContentDirectoryFinder
+    {
+        public static string CalculateContentRootFolder()
+        {
+            var domainAssemblyDirectoryPath =
+                Path.GetDirectoryName(typeof(DomainModule).Assembly.Location);
+            if (domainAssemblyDirectoryPath == null)
+            {
+                throw new Exception(
+                    $"Could not find location of {typeof(DomainModule).Assembly.FullName} assembly!");
+            }
+
+            var directoryInfo = new DirectoryInfo(domainAssemblyDirectoryPath);
+
+            if (Environment.GetEnvironmentVariable("NCrunch") == "1")
+            {
+                while (!DirectoryContains(directoryInfo.FullName, "Washyn.Web.csproj", SearchOption.AllDirectories))
+                {
+                    directoryInfo = directoryInfo.Parent ?? throw new Exception("Could not find content root folder!");
+                }
+
+                var webProject = Directory.GetFiles(directoryInfo.FullName, string.Empty, SearchOption.AllDirectories)
+                    .First(filePath => string.Equals(Path.GetFileName(filePath), "Washyn.Web.csproj"));
+
+                return Path.GetDirectoryName(webProject);
+            }
+
+            while (!DirectoryContains(directoryInfo.FullName, "BasicSolutionAbp.sln"))
+            {
+                directoryInfo = directoryInfo.Parent ?? throw new Exception("Could not find content root folder!");
+            }
+
+            var webFolder = Path.Combine(directoryInfo.FullName, $"src{Path.DirectorySeparatorChar}Washyn.Web");
+            if (Directory.Exists(webFolder))
+            {
+                return webFolder;
+            }
+
+            throw new Exception("Could not find root folder of the web project!");
+        }
+
+        private static bool DirectoryContains(string directory, string fileName,
+            SearchOption searchOption = SearchOption.TopDirectoryOnly)
+        {
+            return Directory.GetFiles(directory, string.Empty, searchOption)
+                .Any(filePath => string.Equals(Path.GetFileName(filePath), fileName));
+        }
+    }
+}
---
+++ test/Washyn.Web.Tests/WebTestStartup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Washyn.Web.Tests
+{
+    public class WebTestStartup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddApplication<WebTestModule>();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.InitializeApplication();
+        }
+    }
+}
