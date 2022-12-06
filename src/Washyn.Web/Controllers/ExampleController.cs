ac329798592d71b2c0834f4dcea339872dc9c7e4
Washington Acero M <shin321654987@gmail.com>
2022-12-06 14:12:11 -0800

    aded api end to end test

---
+++ src/Washyn.Web/Controllers/ExampleController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.AspNetCore.Mvc;
+using Washyn.Application;
+
+namespace Washyn.Web.Controllers
+{
+    // [RemoteService(Name = IdentityRemoteServiceConsts.RemoteServiceName)]
+    // [Area(IdentityRemoteServiceConsts.ModuleName)]
+    // [ControllerName("Example")]
+    [Route("api/example")]
+    public class ExampleController: AbpController, IPruebaAppService
+    {
+        private readonly IPruebaAppService _pruebaAppService;
+
+        public ExampleController(IPruebaAppService pruebaAppService)
+        {
+            _pruebaAppService = pruebaAppService;
+        }
+        
+        [HttpGet]
+        [Route("{id}")]
+        public Task<PruebaDto> GetAsync(long id)
+        {
+            return _pruebaAppService.GetAsync(id);
+        }
+
+        [HttpGet]
+        public Task<PagedResultDto<PruebaDto>> GetListAsync(PagedAndSortedResultRequestDto input)
+        {
+            return _pruebaAppService.GetListAsync(input);
+        }
+
+        [HttpPost]
+        public Task<PruebaDto> CreateAsync(PruebaDto input)
+        {
+            return _pruebaAppService.CreateAsync(input);
+        }
+        
+        [HttpPut]
+        [Route("{id}")]
+        public Task<PruebaDto> UpdateAsync(long id, PruebaDto input)
+        {
+            return _pruebaAppService.UpdateAsync(id, input);
+        }
+        
+        [HttpDelete]
+        [Route("{id}")]
+        public Task DeleteAsync(long id)
+        {
+            return _pruebaAppService.DeleteAsync(id);
+        }
+    }
+}
